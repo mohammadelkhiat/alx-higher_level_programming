e83f6096d500d3a6508ca7ba16f0cc3f9bead160
Mohammad <mohammadelkhiat@gmail.com>
2023-11-29 01:10:22 +0200

    0_0

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-	msg = ""
-	for i in range(0, len(str)):
-		char = str[i]
-		if 'a' <= char <= 'z':
-			msg += '{}'.format(chr(ord(char) - 32))
-		else:
-			msg += char
+    msg = ""
+    for i in range(0, len(str)):
+        char = str[i]
+        if 'a' <= char <= 'z':
+            msg += '{}'.format(chr(ord(char) - 32))
+        else:
+            msg += char
