ff12c23d24404ea43b5c155024b80987526778e9
Mohammad <mohammadelkhiat@gmail.com>
2023-11-28 11:26:49 +0200

    0_0

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,14 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <cstddef>
+#include <stdlib.h>
 
 /**
- * struct listint_s - listint_s
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Description: listint_s
+ * Description: singly linked list node structure
  * 
  */
 typedef struct listint_s
