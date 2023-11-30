99510ac11d27c03997f2b48b34ab68b1e35a7d96
Mohammad <mohammadelkhiat@gmail.com>
2023-12-01 01:09:08 +0200

    0_0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
 
     sin = sys.argv
 
-    NA = len(sys.argv) - 1
+    NA = len(sin) - 1
 
     if NA == 0:
-        print(f"{NA} arguments")
+        print("0 arguments.")
     elif NA == 1:
-        print(f"{NA} argument")
+        print("{} argument:".format(NA))
     else:
-        print(f"{NA} argument")
+        print("{} arguments:".format(NA))
     for i in range(1, NA):
         print(f"{i}: {sin}")
