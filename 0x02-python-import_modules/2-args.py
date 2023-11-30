f5b7c946c4dbff619928925b827e91ee73593d76
Mohammad <mohammadelkhiat@gmail.com>
2023-12-01 00:48:15 +0200

    0_0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     def print_arguments():
         num_arguments = len(sys.argv) - 1
 
-        print(f"Number of argument(s): {num_arguments}", end=' ')
+        print(f"{num_arguments} ", end=' ')
         print("argument" if num_arguments == 1 else "arguments", end='')
         print(":" if num_arguments > 0 else ".")
         for i in range(1, num_arguments + 1):
