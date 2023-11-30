47ce27c39a05a344d8b8e5bac003d0f8bd6defe8
Mohammad <mohammadelkhiat@gmail.com>
2023-12-01 00:23:59 +0200

    0_0

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as yooh
+    
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
