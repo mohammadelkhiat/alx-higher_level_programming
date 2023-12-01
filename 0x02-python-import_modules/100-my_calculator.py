8fda142afc59c235fec83dce87a0b89736e04401
Mohammad <mohammadelkhiat@gmail.com>
2023-12-01 02:38:32 +0200

    0_0

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
    
-    a = int(sys,argv[0])
-    op = sys,argv[1]
+    a = int(sys,argv[1])
+    op = sys,argv[2]
     b = int(sys,argv[3])
     
     if op == "+":
