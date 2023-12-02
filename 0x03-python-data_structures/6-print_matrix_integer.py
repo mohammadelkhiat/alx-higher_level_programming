efb52545b375b0dcd1d67c9c89255cea06940931
Mohammad <mohammadelkhiat@gmail.com>
2023-12-03 00:10:48 +0200

    0_0

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            if i == len(j) - 1:
+                print("{:d}".format(j[i]), end='')
+            else:
+                print("{:d} ".format(j[i]), end='')
+        print()
