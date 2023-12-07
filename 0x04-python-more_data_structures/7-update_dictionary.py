fdbc43ee38040a1a43a5802aad3969eec10141ab
Mohammad <mohammadelkhiat@gmail.com>
2023-12-07 03:17:51 +0200

    0_0

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if a_dictionary[key]:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update(key, value)
