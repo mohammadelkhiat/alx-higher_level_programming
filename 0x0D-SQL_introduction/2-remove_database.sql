99866d9c9448efb44c6845439e5d3ec63f167e14
Mohammad <mohammadelkhiat@gmail.com>
2024-02-15 17:02:07 +0200

    0_0

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,2 @@
--- creates the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+-- delete the database hbtn_0c_0 in your MySQL server
+DROP database IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
--- creates a table called `first_table` in the current database in your MySQL server.
--- `first_table` description:
---        id INT
---        name VARCHAR(256)
--- The database name will be passed as an argument of the mysql command
--- If the table `first_table` already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
+-- a script that prints the full description of the table first_table from the database hbtn_0c_0
+SHOW CREATE table first_table;
