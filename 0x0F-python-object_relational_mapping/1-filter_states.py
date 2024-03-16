adf6d61416f4e8399140a8aee6a8b5c56045fd09
Mohammad <mohammadelkhiat@gmail.com>
2024-03-16 14:00:27 +0200

    0_0

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,11 +5,14 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         user=username,
+                         passwd=password,
+                         db=db_name)
     cur = db.cursor()
     cur.execute("SELECT states.id, name FROM states WHERE name "
                 "COLLATE latin1_general_cs "
