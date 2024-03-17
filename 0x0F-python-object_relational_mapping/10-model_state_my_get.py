4301b1dcae41ed0a834711844bbad9376a443d30
Mohammad <mohammadelkhiat@gmail.com>
2024-03-17 13:50:09 +0200

    0_0

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""mysql state from dayae"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    row = session.query(State).filter(State.name == argv[4]).first()
+
+    if row:
+        print("{}".format(row.id))
+    else:
+        print("Not found")
+
+    session.close()
