c056b480658fa1dbe23806c09c6c05c091840cc3
Mohammad <mohammadelkhiat@gmail.com>
2024-03-17 13:43:35 +0200

    0_0

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""mysql state from dayae"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    row = session.query(State).filter(State.name.like('%a%').all())
+
+    for i in row:
+        print("{}: {}".format(i.id, i.name))
+
+
+    session.close()
